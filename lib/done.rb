dc4c4d84231c8e021791fe9ba9570faf1ba2259d
A Flores <ctrl4ltdeleteme@gmail.com>
2011-02-13 01:45:29 -0500

    added initializers fixed the durn tabbing

--- lib/done.rb
+++ lib/done.rb
@@ -1,22 +1,54 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'done')
 
-%w(config consts).each{|requirement| require requirement}
+%w(config consts yaml fileutils).each{|requirement| require requirement}
 
 module Done
 	class MessageProcessor
 		attr :config
 		attr_writer :config_dir
 
+		include Done::Consts
+
+		class << self
+			def needs_init? dir = Done::Consts::CONFIG_DIR
+				not File.exists? File.join(dir, Done::Consts::CONFIG_FILE)
+			end
+
+			def init options
+				dir = options.delete(:dir) || Done::Consts::CONFIG_DIR
+				conf_file = File.join(dir, Done::Consts::CONFIG_FILE)
+				
+				FileUtils.mkdir_p dir unless File.exists? dir
+				unless File.exists? conf_file
+					File.open(conf_file, 'w') do |f|
+						YAML.dump({ 
+								:owner    => options[:owner], 
+								:api_key  => options[:api_key]
+							}, f)
+					end
+				end
+
+			end
+		end
+ 
 		def initialize config_dir = nil
 			self.config_dir = config_dir
 		end
 
+		def add_project project
+			return if project.nil? or project[:id].nil? or project[:users].nil?
+			config[project[:id]] = project[:users].split(',')
+			config.save config_dir
+			true
+		end
+
 		def config
-      @config ||= Done::Config.load_config self.config_dir
-    end
-    
+			@config ||= Done::Config.load_config self.config_dir
+		end
+
 		def config_dir
-      (@config_dir.nil? && Done::Consts::CONFIG_DIR) || @config_dir
-    end
+			(@config_dir.nil? && Done::Consts::CONFIG_DIR) || @config_dir
+		end
+
 	end
 end
