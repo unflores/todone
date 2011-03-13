3273f104e62fae7aabdf563c9ff611d8d3558d20
A Flores <ctrl4ltdeleteme@gmail.com>
2011-03-12 22:11:27 -0500

    [10622323]add templates for messages sent to the console - moved the pivotal stories view over to the correct spot

--- lib/todone/pivotal_puller.rb
+++ lib/todone/pivotal_puller.rb
@@ -20,26 +20,4 @@ module Todone
 	end
 
 end
-#TODO: merge post-commit into precommit
-#TODO: resque from getaddrinfo: nodename nor servname provided, or not known (SocketError)
-=begin	def last_msg
-		last_commit = `git log -n1`.split("\n")
-		3.times{ last_commit.shift }
-		last_commit_msg = "#\n#==================Last Commit===============\n"
-		last_commit_msg << last_commit.collect{|line| "##{line}"}.join("\n")+"\n"
-	end
-=end
-=begin
-	pre_commit_msg = "#==================Open Tickets================\n"
 
-	unless stories.nil?
-		stories.each do |story|
-			pre_commit_msg << "#[ ##{story['id']}] for #{story['name']}\n"
-		end
-puts pre_commit_msg
-#		File.open("#{ENV['HOME']}/.gitmessage.txt", 'w') do |f|
-#			f.write("\n\n")
-#			f.write(pre_commit_msg)
-#			f.write(last_msg)
-#		end
-=end
--- lib/todone/views.rb
+++ lib/todone/views.rb
@@ -26,6 +26,29 @@ module Todone
 		def updated_pre_commit_hook
 			"Your pre-commit hook has been updated."	
 		end
+		
+		def show_pivotal_stories stories
+			#TODO: merge post-commit into precommit
+			#TODO: resque from getaddrinfo: nodename nor servname provided, or not known (SocketError)
+=begin	def last_msg
+			last_commit = `git log -n1`.split("\n")
+			3.times{ last_commit.shift }
+			last_commit_msg = "#\n#==================Last Commit===============\n"
+			last_commit_msg << last_commit.collect{|line| "##{line}"}.join("\n")+"\n"
+		end
+=end
+			pre_commit_msg = "#==================Open Tickets================\n"
 
+			unless stories.nil?
+				stories.each do |story|
+					pre_commit_msg << "#[ ##{story['id']}] for #{story['name']}\n"
+				end
+				pre_commit_msg
+			end
+		end
+
+		def missing_view method
+			"The method: #{method} does not currently have a view associated with it."
+		end
 	end
 end
